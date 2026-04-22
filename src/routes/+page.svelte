31de95610a1da22341e2340d9c24678eb06d25d7
VN-78 <vishwanath.s.god@gmail.com>
2026-04-22 05:46:42 +0530

    added an basic switch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import Accordion from '../components/molecules/accordion/Accordion.svelte';
+    import Switch from '../components/atoms/buttons/switch/Switch.svelte'
 </script>
 
 <div class="p-4">
 	<Accordion />
+
+    <Switch/>
 </div>
