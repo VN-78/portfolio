81f6d47cf03db1fbe4216e29302b496aa1eac070
VN-78 <vishwanath.s.god@gmail.com>
2026-04-26 18:07:08 +0530

    added an sample avatar from bits ui

---
+++ src/components/atoms/icons/Avatar.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Avatar } from 'bits-ui';
+	import favicon from '$lib/assets/favicon.svg';
+</script>
+
+<Avatar.Root>
+	<Avatar.Image src={favicon} alt="Vishwanaths avatar" />
+	<Avatar.Fallback>VN</Avatar.Fallback>
+</Avatar.Root>
