bde3e5f3b99e44012f2b3827731a7d93a54fc5f4
VN-78 <vishwanath.s.god@gmail.com>
2026-04-27 01:13:12 +0530

    added my image

--- src/components/atoms/icons/Avatar.svelte
+++ src/components/atoms/icons/Avatar.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
 	import { Avatar } from 'bits-ui';
-	import favicon from '$lib/assets/favicon.svg';
+	import vishwa from '$lib/assets/images/main.jpeg';
 </script>
 
 <Avatar.Root>
-	<Avatar.Image src={favicon} alt="Vishwanaths avatar" />
+	<Avatar.Image src={vishwa} alt="Vishwanaths avatar" />
 	<Avatar.Fallback>VN</Avatar.Fallback>
 </Avatar.Root>
